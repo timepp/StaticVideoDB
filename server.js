7f9f2cf8b805d4e5dc41be8337d708a79f5b026b
timepp <timepp@126.com>
2019-11-20 22:49:31 +0800

    fix path bugs

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var url = require('url');
 var querystring = require('querystring');
 var fs = require('fs');
 var glob = require('glob');
+var path = require('path');
 
 var dbdir = process.argv[2];
 
@@ -10,43 +11,44 @@ var server = http.createServer();
 
 server.on("request", function (request, res) {
     let req = url.parse(request.url);
-    let path = decodeURI(req.pathname);
+    let reqpath = decodeURI(req.pathname);
 
-    let fspath = path;
+    let fspath = reqpath;
 
     if (fspath === '/') fspath = "/vdb.html";
 
-    if (path.startsWith("/db/")) {
-        if (path === "/db/filelist.js") {
+    if (reqpath.startsWith("/db/")) {
+        if (reqpath === "/db/filelist.js") {
             glob(`/**/*`, {root:dbdir}, (err,files)=>{
                 let f = files.map(a=>"db" + a.substr(dbdir.length).replace(/\\/g, "/"));
                 let data = "filelist=`\n" + f.join("\n") + "`;";
                 res.setHeader("Cache-Control", "max-age=864000");
                 res.end(data);
-                console.log(`${path} -> ${data.length}`);
+                console.log(`${reqpath} -> ${data.length}`);
             });
             return;
         } else {
-            fspath = dbdir + fspath.substr(3);
-            fspath = fspath.replace(/\//g, "\\");
+            fspath = dbdir + "/" + fspath.substr(3);
         }
     } else {
         fspath = "." + fspath;
     }
 
+    fspath = path.normalize(fspath);
+
     fs.readFile(`${fspath}`, function (err, data) {
         if (err) {
             res.statusCode = 404;
             res.end();
-            console.error(`[404] ${path}`);
+            console.error(`[404] ${reqpath}(${fspath})`);
         } else {
             res.setHeader("Cache-Control", "max-age=864000");
             res.end(data);
-            console.log(`${path} -> ${data.length}`);
+            console.log(`${reqpath}(${fspath}) -> ${data.length}`);
         }
     });
 });
 
-server.listen(3000, function(){
+server.listen(80, function(){
     console.log("server is running");
 });
